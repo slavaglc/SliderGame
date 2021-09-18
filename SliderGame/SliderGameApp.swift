ce278af64854f447681ba952694948fe1cf976c4
slavaglc <47712523+slavaglc@users.noreply.github.com>
2021-09-18 16:58:26 +0300

    Initial Commit

---
+++ SliderGame/SliderGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderGameApp.swift
+//  SliderGame
+//
+//  Created by slava on 18.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct SliderGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
